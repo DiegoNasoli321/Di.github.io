7f4a8fef538807de1cdd4ef74a1d5907bbea3746
DiegoNasoli321 <151102062+DiegoNasoli321@users.noreply.github.com>
2023-12-31 21:26:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const envio = document.getElementById("botao");
 envio.addEventListener("click", function(){
     const campo = document.querySelector("#campo");
     const numero = campo.value;
-    sessionStorage.setItem("Numero ->",numero);
+    console.log("Numero ->",numero);
     const apagartexto = document.querySelector("#campo");
     apagartexto.value=""
 }
